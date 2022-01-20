3b67d38b389fc026e630f1c0f1c8186d6f9d0840
Andnash <atn22@byui.edu>
2022-01-19 20:33:22 -0500

    java date

--- lesson1/js/jstemplate.js
+++ lesson1/js/jstemplate.js
@@ -1,29 +1,5 @@
-/*var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+const last_mod = document.querySelector('#last_mod');
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting  = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!';
-} else {
-    greeting = 'Welcome!';
-}
-
-document.write('<h3>' + greeting + '</h3>');
-
-or 
-let last_mod = document.getElementById('last_mod');
-
-or
-var string = document.lastModified; 
-  
-}*/
-
-
-let last_mod = new Date(document.lastModified);
-  
+last_mod.textContent = 'Last Updated: ${document.lastModified}`;
 
 
---
+++ lesson1/js/lastupdate.js
@@ -0,0 +1,4 @@
+const last_mod = document.querySelector('#last_mod');
+
+last_mod.textContent = 'Last Updated: ${document.lastModified}';
+
